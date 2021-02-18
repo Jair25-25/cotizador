cdac5fa8d54a331192b292df7299ed526d69404d
Jair25 <jairtronik@gmail.com>
2021-02-18 14:32:39 -0500

    Cotizador

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import propTypes from 'prop-types';
+
+const ContenedorHeader = styled.header`
+    background-color: #0B3954;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
+
+const Header = ({ titulo }) => {
+    return (
+        <ContenedorHeader>
+            <TextoHeader>{ titulo }</TextoHeader>
+        </ContenedorHeader>
+    );
+}
+
+Header.propTypes = {
+    titulo: propTypes.string.isRequired
+    
+}
+
+export default Header;
